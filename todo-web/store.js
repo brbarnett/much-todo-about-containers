6e4b5236e926ccf71539c58d2e8d32951432ecb8
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-04-28 18:08:33 -0500

    Http service

--- todo-web/store.js
+++ todo-web/store.js
@@ -1,10 +1,39 @@
 const axios = require('axios');
-const service = mockService();
+const service = httpService();
 
 module.exports = todoStorage = {
     fetch: service.getAll,
     save: service.save
 };
+
+function httpService() {
+    return {
+        getAll: getAll,
+        save: save
+    };
+
+    function getAll() {
+        return new Promise(function (resolve, reject) {
+            console.log('getting all todos');
+            axios
+                .get("http://localhost:8082")
+                .then(resp => {
+                    resolve(resp.data);
+                })
+        });
+    }
+
+    function save(todos) {
+        return new Promise(function (resolve, reject) {
+            console.log('getting all todos');
+            axios
+                .post("http://localhost:8082", todos)
+                .then(resp => {
+                    resolve(null);
+                })
+        });
+    }
+}
 
 function mockService() {
     let data = [{ "id": 1, "title": "this one", "completed": false }, { "id": 2, "title": "that one", "completed": false }, { "id": 3, "title": "another", "completed": false }, { "id": 4, "title": "here", "completed": false }];
