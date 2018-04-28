5c0421663fc0e70951e57ba86ada18c696e73b50
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-04-28 18:08:47 -0500

    Data mock in API

--- todo-api/app.js
+++ todo-api/app.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(cors())
+app.use(bodyParser.json());
+
+let data = [{ "id": 1, "title": "this one", "completed": false }, { "id": 2, "title": "that one", "completed": false }, { "id": 3, "title": "another", "completed": false }, { "id": 4, "title": "here", "completed": false }];
 
 app.get('/', function (req, res) {
-    res.send({ content: 'do this' });
+    res.send(data);
 });
 
-app.listen(3000);
+app.post('/', function(req, res){
+    data = req.body;
+
+    res.sendStatus(200);
+});
+
+app.listen(3000); 
