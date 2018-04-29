7611c04567379e30499dbe34cb3611b69a46c5d5
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-04-29 09:56:22 -0500

    ENV, ports, proxy work

--- todo-api/app.js
+++ todo-api/app.js
@@ -2,20 +2,29 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
+var port = process.env.PORT || 80;
 
 app.use(cors())
 app.use(bodyParser.json());
 
 let data = [{ "id": 1, "title": "this one", "completed": false }, { "id": 2, "title": "that one", "completed": false }, { "id": 3, "title": "another", "completed": false }, { "id": 4, "title": "here", "completed": false }];
 
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
     res.send(data);
 });
 
-app.post('/', function(req, res){
+app.post('/api', function(req, res){
     data = req.body;
 
     res.sendStatus(200);
 });
 
-app.listen(3000); 
+// catch all 404
+app.all('*', function(req, res){
+    res.sendStatus(404);
+})
+
+console.log('starting application');
+app.listen(port, function() {
+    console.log('listening on', port);
+}); 
