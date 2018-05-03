f7323ec5cb34fc69aeef4dceec761fe9b3ae89f4
Brandon R Barnett <BBarnett@rightpoint.com>
2018-05-03 12:42:03 -0500

    Configuring env vars

--- todo-api/app.js
+++ todo-api/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(bodyParser.json());
