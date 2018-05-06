a3b96c236b952142f9c66d0f6781a94813e5bc07
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2018-05-06 10:13:55 -0500

    Changing app to use / instead of /api

--- todo-api/app.js
+++ todo-api/app.js
@@ -7,11 +7,11 @@ app.use(bodyParser.json());
 
 let data = [{ "id": 1, "title": "this one", "completed": false }, { "id": 2, "title": "that one", "completed": false }, { "id": 3, "title": "another", "completed": false }, { "id": 4, "title": "here", "completed": false }];
 
-app.get('/api', function (req, res) {
+app.get('/', function (req, res) {
     res.send(data);
 });
 
-app.post('/api', function(req, res){
+app.post('/', function(req, res){
     data = req.body;
  
     res.sendStatus(200); 
