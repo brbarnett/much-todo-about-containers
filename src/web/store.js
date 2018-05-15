638b3dcc334623a284954467e7058cb893f5be5d
Brandon R Barnett <BBarnett@rightpoint.com>
2018-05-15 16:24:28 -0500

    Removing mock service

--- src/web/store.js
+++ src/web/store.js
@@ -35,26 +35,3 @@ function httpService() {
         });
     }
 }
-
-function mockService() {
-    let data = [{ "id": 1, "title": "this one", "completed": false }, { "id": 2, "title": "that one", "completed": false }, { "id": 3, "title": "another", "completed": false }, { "id": 4, "title": "here", "completed": false }];
-
-    return {
-        getAll: getAll,
-        save: save
-    };
-
-    function getAll() {
-        return new Promise(function (resolve, reject) {
-            setTimeout(resolve, 100, data);
-        });
-    }
-
-    function save(todos) {
-        return new Promise(function (resolve, reject) {
-            data = todos;
-
-            setTimeout(resolve, 100, null);
-        })
-    }
-}
